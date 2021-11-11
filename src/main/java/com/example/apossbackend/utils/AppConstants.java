160c63b7cb74ceda9f65660a2fc04e89795e0904
Bùi Dương Duy Khang <81902132+khangbdd@users.noreply.github.com>
2021-11-11 20:14:33 +0700

    wrong branch

--- src/main/java/com/example/apossbackend/utils/AppConstants.java
+++ src/main/java/com/example/apossbackend/utils/AppConstants.java
@@ -2,6 +2,7 @@ package com.example.apossbackend.utils;
 
 public class AppConstants {
 
+    public static final String DEFAULT_KEYWORD = "";
     public static final String DEFAULT_PAGE_NUMBER = "1";
     public  static final String DEFAULT_PAGE_SIZE = "10";
     public static final String DEFAULT_SORT_BY = "id";
